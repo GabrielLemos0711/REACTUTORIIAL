067fea053caf71f8e1e04bef551c70d4c50a5ed4
Gabriel.Lemos <104501418+GabrielLemos0711@users.noreply.github.com>
2023-03-01 09:13:31 -0300

    content e footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css'
+import Box from '@mui/material/Box'
+
+const Footer = () => {
+    return (
+        <Box component='Footer'>
+            <h3>© Copyright a Gabriel Lemos</h3>
+        </Box>
+    )
+}
+
+export default Footer
